3c263f227bc544bf4888e41ff1ee2867f9b63629
DESKTOP-1T14MEC\X555L <eer87912@gmail.com>
2022-07-15 16:33:37 +0800

    dailovescore

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+var score = prompt("寶寶 請問你對我的愛有幾分(･8･)")
+    if(score>=90){
+       alert("Me too (手比愛心)");
+    }
+    else if (score<90 && score>=80){
+       alert("好啦愛你喔");
+    }
+    else if (score<80 && score>=70){
+       alert("哪裡不滿意???");
+    }
+    else if (score<70 && score>=60){
+       alert("你不愛我QQ");
+    }
+    else{
+       alert("輸入這個分數你良心不痛嗎(`3´)");
+    }
+    
